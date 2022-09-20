e22caa843ed366d23dc8c4aebc81cb63487b0d25
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-09-20 16:18:50 -0400

    deployment sucessful, profile picture appears on the about me page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProPic from '../../img/profilepicture.png'
 
 export default function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <img className='profilePic' src="../img/profilepicture.png" alt="profile_picture"/>
+      <img className='profilePic' src={ProPic} alt="profile_picture"/>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
