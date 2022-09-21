fc45e2c8872ff03b5fe5253de4f71d248a7418d1
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-09-21 19:09:01 -0400

    about me page updated

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -7,14 +7,11 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <img className='profilePic' src={ProPic} alt="profile_picture"/>
+      <h2>Hi my name is Fred Kamm</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        I am a certifed Full-Stack Web Developer located in Bucks County, PA. I have a serious passion in creating front end applications for ui effects, animations, and creating dynamic user experience. My priorities consist of creating applications with fast load times with lag free interactions. My layouts are mobile responive with an easy to use UX/UI.
       </p>
+      <h3>I love making web pages come to life </h3>
     </div>
   );
 }
