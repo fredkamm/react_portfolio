c5bb6fc233eef64ac25bbcad817d02e9cd5d1080
fredkamm <fred.kamm95@gmail.com>
2023-04-18 21:39:17 -0400

    styling update

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,13 @@ module.exports = {
         xl: '1280px',
       },
       extend: {
-        content: {
-          about: 'url("/src/assets/img/outline-text/about.svg")',
-          portfolio: 'url("/src/assets/img/outline-text/portfolio.svg")',
-          services: 'url("/src/assets/img/outline-text/services.svg")',
-          testimonials: 'url("/src/assets/img/outline-text/testimonials.svg")',
-          contact: 'url("/src/assets/img/outline-text/contact.svg")',
-        },
+        // content: {
+        //   about: 'url("/src/assets/img/outline-text/about.svg")',
+        //   portfolio: 'url("/src/assets/img/outline-text/portfolio.svg")',
+        //   services: 'url("/src/assets/img/outline-text/services.svg")',
+        //   testimonials: 'url("/src/assets/img/outline-text/testimonials.svg")',
+        //   contact: 'url("/src/assets/img/outline-text/contact.svg")',
+        // },
         colors: {
           primary: '#050402',
           secondary: '#1C1D24',
